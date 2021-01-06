92f13f0a30624d0bf331018b0ef6a1b2eec293b1
mikehwagz <mike@selfaware.studio>
2021-01-06 13:25:59 -0500

    update squeezebox for default index feature

--- web/src/scripts/components/accordion.js
+++ web/src/scripts/components/accordion.js
@@ -2,10 +2,14 @@ import { component } from 'picoapp'
 import squeezebox from '@/lib/squeezebox'
 
 export default component((node, ctx) => {
-  const accordion = squeezebox(node, { openMultiple: true })
+  const accordion = squeezebox(node, {
+    defaultIndex: 0,
+    collapsible: true,
+    resize: false,
+  })
 
   ctx.on('resize', accordion.resize)
-  accordion.resize()
+  accordion.mount()
 
   return () => {
     accordion.unmount()
--- web/src/scripts/lib/squeezebox.js
+++ web/src/scripts/lib/squeezebox.js
@@ -1,110 +1,122 @@
-import choozy from 'choozy'
-import { on, add, remove, has } from 'martha'
+import smitter from 'smitter'
+import { qsa, on } from 'martha'
 
 export default function squeezebox(
   node,
-  { openMultiple = false, strictMode = -1, autoResize = false } = {},
+  {
+    multiple = false,
+    collapsible = false,
+    defaultIndex = null,
+    resize = true,
+  } = {},
 ) {
-  const { headers, panels, inners } = allToArray(choozy(node))
-  let offResize = null
-  const fns = []
-
-  setAttributes(headers, panels)
-
-  if (autoResize) {
-    resize()
-    offResize = on(window, 'resize', resize)
-  }
-
-  let offClick = on(headers, 'click', (ev) => {
-    const header = ev.currentTarget
-    const index = parseInt(header.dataset.index)
-    const panel = panels[index]
-
-    const otherHeaders = removeIndexFromArray(headers, index)
-    const otherPanels = removeIndexFromArray(panels, index)
-
-    if (has(panel, 'is-hidden')) {
-      expand(header, panel)
-
-      fns.forEach((fn) => fn({ type: 'expand', index, header, panel }))
-
-      if (openMultiple) return
-
-      otherHeaders.forEach((header, i) => {
-        collapse(header, otherPanels[i])
-      })
-    } else {
-      if (strictMode > -1) return
-
-      collapse(header, panel)
-
-      fns.forEach((fn) => fn({ type: 'collapse', index, header, panel }))
-    }
-  })
-
-  function expand(header, panel) {
-    let off = on(panel, 'transitionend', () => {
-      off()
-      fns.forEach((fn) => fn({ type: 'transitionend', header, panel }))
-    })
-
-    header.setAttribute('aria-expanded', true)
-    remove(panel, 'is-hidden')
-  }
-
-  function collapse(header, panel) {
-    header.setAttribute('aria-expanded', false)
-    add(panel, 'is-hidden')
-  }
-
-  function setAttributes(headers, panels) {
-    headers.forEach((header, i) => {
-      header.setAttribute('aria-controls', `panel-${i}`)
-      header.setAttribute('id', `header-${i}`)
-      header.setAttribute('data-index', i)
-      header.setAttribute('aria-expanded', strictMode > -1 && strictMode === i)
-    })
-
-    panels.forEach((panel, i) => {
-      panel.setAttribute('role', 'region')
-      panel.setAttribute('aria-labelledby', `header-${i}`)
-      panel.setAttribute('id', `panel-${i}`)
-
-      if (strictMode > -1 && i === strictMode) return
-
-      add(panel, 'is-hidden')
-    })
-  }
+  let offResize = () => {}
+  let uid = Date.now()
+  let emitter = smitter()
+  let events = []
+  let select = (name) => qsa(`[data-squeezebox-${name}]`, node)
+  let headers = select('header')
+  let panels = select('panel')
+  let inners = select('panel-inner')
+  let items = headers.map((header, i) => ({
+    index: i,
+    header,
+    panel: panels[i],
+    inner: inners[i],
+    expanded: i === defaultIndex,
+  }))
 
   function resize() {
-    panels.forEach((panel, i) => {
-      panel.style.maxHeight = inners[i].clientHeight + 'px'
+    items.forEach(({ panel, inner }) => {
+      panel.style.maxHeight = inner.clientHeight + 'px'
     })
   }
 
-  function removeIndexFromArray(array, index) {
-    const left = array.slice(0, index)
-    const right = array.slice(index + 1)
-    return left.concat(right)
+  function expand(item) {
+    let { header, panel } = item
+
+    let off = on(panel, 'transitionend', () => {
+      off()
+      emitter.emit('expanded', item)
+    })
+
+    item.expanded = true
+    header.setAttribute('aria-expanded', true)
+    panel.removeAttribute('aria-hidden')
+
+    emitter.emit('expand', item)
+  }
+
+  function collapse(item, initial) {
+    let { header, panel } = item
+
+    if (!initial) {
+      let off = on(panel, 'transitionend', () => {
+        off()
+        emitter.emit('collapsed', item)
+      })
+    }
+
+    item.expanded = false
+    header.setAttribute('aria-expanded', false)
+    panel.setAttribute('aria-hidden', true)
+
+    !initial && emitter.emit('collapse', item)
+  }
+
+  function setup(item) {
+    let { header, panel, expanded, index } = item
+
+    header.setAttribute('aria-controls', `panel-${uid}-${index}`)
+    header.setAttribute('id', `header-${uid}-${index}`)
+
+    panel.setAttribute('role', 'region')
+    panel.setAttribute('aria-labelledby', `header-${uid}-${index}`)
+    panel.setAttribute('id', `panel-${uid}-${index}`)
+
+    if (expanded) {
+      expand(item)
+    } else {
+      collapse(item, true)
+    }
   }
 
   return {
+    on: emitter.on,
     resize,
-    unmount() {
-      autoResize && offResize()
-      offClick()
+    mount() {
+      if (resize) {
+        offResize = on(window, 'resize', resize)
+        resize()
+      }
+
+      items.forEach((item, i) => {
+        setup(item)
+
+        let off = on(item.header, 'click', () => {
+          let otherItems = removeIndexFromArray(items, i)
+
+          if (!item.expanded) {
+            expand(item)
+            if (multiple) return
+            otherItems.filter((item) => item.expanded).map(collapse)
+          } else {
+            collapsible && collapse(item)
+          }
+        })
+
+        events.push(off)
+      })
     },
-    onChange(fn) {
-      fns.push(fn)
-      return () => fns.splice(fns.indexOf(fn), 1)
+    unmount() {
+      offResize()
+      events.map((off) => off())
     },
   }
 }
 
-function allToArray(obj) {
-  return Object.entries(obj).reduce((acc, [key, val]) => {
-    acc[key] = Array.isArray(val) ? val : [val]
-    return acc
-  }, {})
+function removeIndexFromArray(array, index) {
+  const left = array.slice(0, index)
+  const right = array.slice(index + 1)
+  return left.concat(right)
 }
