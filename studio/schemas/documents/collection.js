230696fb177893e5409367b19d41b332797b79c9
mikehwagz <mike@selfaware.studio>
2021-10-06 14:54:23 -0400

    add fields for new homepage to sanity schema

--- studio/schemas/documents/collection.js
+++ studio/schemas/documents/collection.js
@@ -2,6 +2,7 @@ export default {
   title: 'Collection',
   name: 'collection',
   type: 'document',
+  __experimental_actions: ['update', 'publish'],
   fields: [
     {
       title: 'Title',
--- studio/schemas/documents/homepage.js
+++ studio/schemas/documents/homepage.js
@@ -2,8 +2,20 @@ export default {
   title: 'Homepage',
   name: 'homepage',
   type: 'document',
-  // __experimental_actions: ['update', 'publish'],
+  __experimental_actions: ['update', 'publish'],
   fields: [
+    {
+      name: 'slideshow',
+      title: 'Slideshow',
+      type: 'array',
+      of: [{ type: 'a11yImage' }, { type: 'video' }],
+    },
+    {
+      name: 'introText',
+      title: 'Intro Text',
+      type: 'text',
+      rows: 2,
+    },
     {
       title: 'Products',
       name: 'products',
