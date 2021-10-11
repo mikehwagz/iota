70e541f5fe5a5e6839c573f8849038c845527ce7
mikehwagz <mike@selfaware.studio>
2021-10-11 10:03:08 -0400

    mobile carousel

--- web/src/scripts/components/productThumb.js
+++ web/src/scripts/components/productThumb.js
@@ -3,16 +3,16 @@ import choozy from 'choozy'
 import { on, add, remove, wrap } from 'martha'
 
 export default component((node, ctx) => {
-  let { images } = choozy(node)
+  let { images, link } = choozy(node)
   let delay = 300
   let id = null
   let current = 1
 
-  let offEnter = on(node, 'mouseenter', () => {
+  let offEnter = on(link, 'mouseenter', () => {
     step(current)
   })
 
-  let offLeave = on(node, 'mouseleave', () => {
+  let offLeave = on(link, 'mouseleave', () => {
     id && clearTimeout(id)
   })
 
