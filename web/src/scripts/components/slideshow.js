393808986c74561c29642c1d4ea277d8aa2f5303
mikehwagz <mike@selfaware.studio>
2021-10-11 09:46:01 -0400

    desktop homepage carousel

--- web/src/scripts/components/slideshow.js
+++ web/src/scripts/components/slideshow.js
@@ -1,23 +1,29 @@
 import { component } from 'picoapp'
 import choozy from 'choozy'
-import { index, on, add, remove } from 'martha'
+import { index, on, add, remove, wrap } from 'martha'
+import signal from '../lib/signal'
 
 export default component((node, ctx) => {
-  let { slides, btns, indicators } = choozy(node)
+  let { slides, btns, next, prev, indicators } = choozy(node)
 
-  on(btns, 'click', ({ currentTarget: t }) => {
-    let selectedIndex = index(t)
-
-    indicators.forEach((indicator, i) => {
-      let slide = slides[i]
-
-      if (i === selectedIndex) {
-        remove(indicator, 'o0')
+  let [idx, set] = signal(0, (idx) => {
+    slides.forEach((slide, i) => {
+      let indicator = indicators[i]
+      if (i === idx) {
         remove(slide, 'dn')
+        remove(indicator, 'o0')
       } else {
-        add(indicator, 'o0')
         add(slide, 'dn')
+        add(indicator, 'o0')
       }
     })
   })
+
+  let events = [
+    on(prev, 'click', () => set(wrap(idx() - 1, btns.length))),
+    on(next, 'click', () => set(wrap(idx() + 1, btns.length))),
+    on(btns, 'click', (e) => set(index(e.currentTarget))),
+  ]
+
+  return () => events.map((off) => off())
 })
