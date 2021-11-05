17f5b438489eacbed2bdee982b9d552bbead8d42
mikehwagz <mike@selfaware.studio>
2021-11-05 10:26:58 -0400

    chore(studio): upgrades and netlify dash

--- studio/dashboard.js
+++ studio/dashboard.js
@@ -1,18 +1,18 @@
 export default {
   widgets: [
-    // {
-    //   name: 'netlify',
-    //   options: {
-    //     title: 'Netlify',
-    //     sites: [
-    //       {
-    //         title: '<SITE_TITLE>',
-    //         apiId: '<NETLIFY_API_ID>',
-    //         buildHookId: '<NETLIFY_BUILD_HOOK_ID>',
-    //         name: '<NETLIFY_SITE_NAME>',
-    //       },
-    //     ],
-    //   },
-    // },
+    {
+      name: 'netlify',
+      options: {
+        title: 'Netlify',
+        sites: [
+          {
+            title: 'Production',
+            apiId: 'f2b08e32-5f5c-4dc4-9ae1-4680f5579f6a',
+            buildHookId: '6004a0c171e7dfcc52c97992',
+            name: 'iota-shop',
+          },
+        ],
+      },
+    },
   ],
 }
